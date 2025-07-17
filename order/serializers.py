7140ab160dfed886f5df58f4642d389b82ff6d9e
=shikha <=shikhakumari06578@gmail.com>
2025-07-17 23:58:57 +0900

    sending image in cart list API

--- order/serializers.py
+++ order/serializers.py
@@ -7,7 +7,7 @@ from tours.models import Tour
 class TourSummarySerializer(serializers.ModelSerializer):
     class Meta:
         model = Tour
-        fields = ['uuid', 'title', 'price', 'main_image', 'tour_type', 'about']  # remove 'quantity'
+        fields = ['uuid', 'title', 'price', 'main_image', 'tour_type', 'duration','short_description']  # remove 'quantity'
 
 
 class CartItemSerializer(serializers.ModelSerializer):
